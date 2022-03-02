b6454c5a1a06ffffd3f28aa0890521d53fa7e022
Corrado.Ferra <corradoferra@gmail.com>
2022-03-02 19:02:28 +0100

    button

--- resources/views/cliccami.blade.php
+++ resources/views/cliccami.blade.php
@@ -8,5 +8,14 @@
 </head>
 <body>
     <h1>Bravo, hai cliccato!</h1>
+    <h2>vai in fondo alla pagina per capire quanto sei bravo!</h2>
+    @for ($i = 0; $i < 2000; $i++ )
+        <span>BRAVO!</span>
+    @endfor
+    
+    <div><h1>Bravo {{ $i }} volte! </h1></div>
+    
+
+
 </body>
 </html>
